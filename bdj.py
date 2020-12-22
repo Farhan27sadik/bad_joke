5a18c1707eb8d6928966054fd50f696022029852
Farhan27sadik <52166898+Farhan27sadik@users.noreply.github.com>
2020-12-23 05:01:10 +0600

    Add files via upload

---
+++ bdj.py
@@ -0,0 +1,31 @@
+''''
+
+import pyttsx3
+import PyPDF2
+
+book = open('oop.pdf', 'rb')
+pdfReader = PyPDF2.PdfFileReader(book)
+pages = pdfReader.numPages
+
+speaker = pyttsx3.init()
+for num in range(7, pages):
+    page = pdfReader.getPage(num)
+    text = page.extractText()
+    speaker.say(text)
+    speaker.runAndWait()
+
+'''
+
+
+
+import pyautogui
+import time
+m=100
+while m>0:
+    time.sleep(1)
+    pyautogui.typewrite('You have challenged a wrong person!\n')
+    pyautogui.press('enter')
+    m=m-1
+
+
+
